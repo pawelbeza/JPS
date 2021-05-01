a8fc4a51b9e6a7bbc5ea81ea1f6d8336478b7acc
Paweł Bęza <pawel.beza99@gmail.com>
2021-05-01 12:00:30 +0200

    interactive processing order choice & steps number constraint

--- 4-Kod_zrodl_do_kompilacji.pl
+++ 4-Kod_zrodl_do_kompilacji.pl
@@ -1,30 +1,70 @@
-start_A_star( InitState, PathCost) :-
+start_A_star( InitState, MaxStepsNum, NNodesNum, PathCost) :-
 	score(InitState, 0, 0, InitCost, InitScore) ,
-	search_A_star( [node(InitState, nil, nil, InitCost , InitScore ) ], [ ], PathCost) .
+	search_A_star( [node(InitState, nil, nil, InitCost , InitScore ) ], 1, MaxStepsNum, NNodesNum, [ ], PathCost) .
+
+search_A_star(Queue, StepCounter, MaxStepsNum, NNodesNum, ClosedSet, PathCost) :-
+	StepCounter > MaxStepsNum, !,
+	write('Step nr: '), write(StepCounter), nl,
+	write('Steps number exceed steps limit, do you want to increase it? (y/n)\n'),
+	read('y'),
+	write('How many steps add to limit?\n'),
+	read(AddToLimit),
+	NewMaxStepsNum is MaxStepsNum + AddToLimit,
+	search_A_star(Queue, StepCounter, NewMaxStepsNum, NNodesNum, ClosedSet, PathCost).
+
+search_A_star(Queue, StepCounter, MaxStepsNum, NNodesNum, ClosedSet, PathCost) :-
+	write('Step nr: '), write(StepCounter), nl,
+	NewStepCounter is StepCounter + 1,
+	write('Top '), write(NNodesNum), write(' nodes with smallest heuristic value\n'),
+	write_n_nodes(NNodesNum, Queue),
+	write('Enter nodes order\n'),
+	read(NNodes),
+	fetch_node_index(NNodes, NodeIndex),
+	fetch(Node, NodeIndex, Queue, ClosedSet , RestQueue),
+	write('\nYou\'re currently at node:\n'),
+	write_node(Node), nl,
+	continue(Node, NewStepCounter, MaxStepsNum, NNodesNum,RestQueue, ClosedSet, PathCost).
 
 
-search_A_star(Queue, ClosedSet, PathCost) :-
-	fetch(Node, Queue, ClosedSet , RestQueue),
-	continue(Node, RestQueue, ClosedSet, PathCost).
-
-
-continue(node(State, Action, Parent, Cost, _), _, ClosedSet, path_cost(Path, Cost) ) :-
+continue(node(State, Action, Parent, Cost, _), _, _, _, _, ClosedSet, path_cost(Path, Cost) ) :-
 	goal( State), !,
 	build_path(node(Parent, _ ,_ , _ , _ ) , ClosedSet, [Action/State], Path) .
 
-continue(Node, RestQueue, ClosedSet, Path)   :-
+continue(Node, StepCounter, MaxStepsNum, NNodesNum, RestQueue, ClosedSet, Path)   :-
 	expand( Node, NewNodes),
 	insert_new_nodes(NewNodes, RestQueue, NewQueue),
-	search_A_star(NewQueue, [Node | ClosedSet ], Path).
+	search_A_star(NewQueue, StepCounter, MaxStepsNum, NNodesNum, [Node | ClosedSet ], Path).
 
 
-fetch(node(State, Action,Parent, Cost, Score),
-			[node(State, Action,Parent, Cost, Score) |RestQueue], ClosedSet,  RestQueue) :-
-	\+ member(node(State, _, _, _, _), ClosedSet), !.
+write_node(node(State, Action, _, _, Score)) :-
+	write('state: '), write(State), nl,
+	write('action: '), write(Action), nl,
+	write('total heuristic cost: '), write(Score), nl.
 
-fetch(Node, [ _ |RestQueue], ClosedSet, NewRest) :-
-	fetch(Node, RestQueue, ClosedSet , NewRest).
+write_n_nodes(_, []) :- !.
 
+write_n_nodes(NodesNum, _) :-
+	NodesNum =< 0, !.
+
+write_n_nodes(NodesNum, [Node | RestQueue]) :-
+	NewNodesNum is NodesNum - 1,
+	write_node(Node),
+	write('--------------------------'), nl,
+	write_n_nodes(NewNodesNum, RestQueue).
+
+fetch_node_index([NodeIndex | _], NodeIndex).
+
+fetch_node_index([_ | Rest], NodeIndex) :-
+	fetch_node_index(Rest, NodeIndex).
+	
+fetch(node(State, Action,Parent, Cost, Score), 
+			Ind, [node(State, Action,Parent, Cost, Score) | RestQueue], ClosedSet, RestQueue) :-
+	Ind == 0,
+	\+ member(node(State, _, _, _, _), ClosedSet).
+
+fetch(Node, Ind, [Y | Queue], ClosedSet, [Y | RestQueue]) :-
+	NewInd is Ind - 1,
+	fetch(Node, NewInd, Queue, ClosedSet, RestQueue).
 
 expand(node(State, _ ,_ , Cost, _ ), NewNodes)  :-
 	findall(node(ChildState, Action, State, NewCost, ChildScore) ,
@@ -71,23 +111,33 @@ del([Y|R],X,[Y|R1]) :-
 	del(R,X,R1).
 	
 
-succ(a, ab, 2, b).
-succ(b, bf, 3, f).
-succ(a, ac, 3, c).
-succ(b, bg, 4, g).
-succ(g, gm, 2, m).
-succ(c, cd, 2, d).
-succ(d, dm, 2, m).
-succ(c, ce, 3, e).
-succ(e, em, 5, m).
+succ(a, ab, 3, b).
+succ(a, aj, 4, j).
+succ(a, ag, 1, g).
+succ(b, bd, 10, d).
+succ(j, jd, 3, d).
+succ(j, jg, 6, g).
+succ(g, ge, 14, e).
+succ(g, gf, 8, f).
+succ(d, dh, 11, h).
+succ(e, ef, 2, f).
+succ(f, fh, 4, h).
+succ(e, ei, 1, i).
+succ(f, fi, 2, i).
+succ(i, ic, 10, c).
+succ(i, ih, 6, h).
+succ(h, hc, 3, c).
+succ(i, ig, 5, g).
 
-goal(m).
+goal(c).
 
-hScore(a, 4).
-hScore(b, 4).
-hScore(f, 7).
-hScore(g, 1).
-hScore(m, 0).
-hScore(c, 3).
-hScore(d, 1).
-hScore(e, 4).
+hScore(a, 15).
+hScore(b, 20).
+hScore(c, 0).
+hScore(d, 13).
+hScore(e, 20).
+hScore(f, 5).
+hScore(g, 13).
+hScore(h, 1).
+hScore(i, 6).
+hScore(j, 15).
